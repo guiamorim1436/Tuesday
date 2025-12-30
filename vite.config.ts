9af5c9529bf5505290df743aa4168967a4bb34e7
guiamorim1436 <gui.amorim1436@gmail.com>
2025-12-29 22:57:50 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  return {
   base: './', // Garante caminhos relativos para assets no build de produção
   server: {
     host: true,
